31837fb87c9503dd5d5abc51695b914525deaaf3
Alexander-DA <alex.alexandrov200@gmail.com>
2024-09-23 13:21:59 +0300

    Create movie functionality

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,14 @@
-import movieData from "../data/movieData.js"
+import movieData from "../data/movieData.js";
+import uniqId from 'uniqid';
+ 
+const getAll = () => movieData.getAll();
 
-const getAll = () => movieData.getMovies();
+const create = (movie) => {
+    movie.id = uniqId();
+    return movieData.create(movie);
+}
 
 export default {
     getAll,
+    create
 }
