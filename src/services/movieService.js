569f3b7b8481435d31e6e3985b284be5c6b81945
Alexander-DA <alex.alexandrov200@gmail.com>
2024-09-29 16:40:49 +0300

    Refactor data layer to use mongoose models

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,8 +1,8 @@
-import movieData from "../data/movieData.js";
-import uniqId from 'uniqid';
+import Movie from "../models/movie.js";
  
+// TODO: Refactor using db filtration 
 const getAll = async (filter = {}) => {
-    let movies = await movieData.getAll();
+    let movies = await Movie.find();
 
     if (filter.search) {
         movies = movies.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()))
@@ -19,19 +19,9 @@ const getAll = async (filter = {}) => {
     return movies;
 }
 
-const create = (movie) => {
-    movie.id = uniqId();
-    movie.rating = Number(movie.rating);
-    return movieData.create(movie);
-}
+const create = (movie) => Movie.create(movie);
 
-const getOne = async (movieId) => {
-    const movies = await movieData.getAll();
-
-    const resultMovie = movies.find(movie => movie.id == movieId);
-
-    return resultMovie;
-}
+const getOne = (movieId) => Movie.findById(movieId);
 
 export default {
     getAll,
