e3d6cedeb921a9588b4307d1e548498b1ec9ecfc
Alexander-DA <alex.alexandrov200@gmail.com>
2024-09-30 18:47:01 +0300

    Fixed cast filtering

--- src/services/castService.js
+++ src/services/castService.js
@@ -2,7 +2,8 @@ import Cast from "../models/Cast.js";
 
 const getAll = () => Cast.find();
 
-const getAllWithout = (castIds) => Cast.find({ _id: { $nin: castIds } })
+// const getAllWithout = (castIds) => Cast.find({ _id: { $nin: castIds } })
+const getAllWithout = (castIds) => Cast.find().nin('_id', castIds);
 
 const create = (cast) => Cast.create(cast);
 
